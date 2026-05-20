a41b770b5a4a327766c08c02ca53072789661dcb
matheuspsfranco <mathfort991@gmail.com>
2026-05-20 00:00:12 -0300

    feat: Js da página Time

---
+++ assets/js/Time.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const menuToggle = document.getElementById('menuToggle');
+  const navMobile = document.getElementById('navMobile');
+ 
+  if (menuToggle && navMobile) {
+    menuToggle.addEventListener('click', () => {
+      menuToggle.classList.toggle('active');
+      navMobile.classList.toggle('active');
+    });
+  }
+});
